f2eebf15b77702b15471324aa4f49037f9dc4bc5
Heitor-hub-del <heitortrader2023@gmail.com>
2026-01-25 09:38:39 -0300

    1.1 melhorias e adições: adicionado 2 novos operadores e um mini sistema de loop e um novo jeito de sair do programa

--- calculo.py
+++ calculo.py
@@ -1,12 +1,12 @@
 print("bem vindo a calculadora realista!")
 
-input1 = input("qual calculo voce deseja fazer?  1.soma 2.subtração 3.multiplicação 4.divisão: ")
+input1 = input("qual calculo voce deseja fazer?  1.soma 2.subtração 3.multiplicação 4.divisão 5.potencialização,6. fazer um loop, 7.sair : ")
 
 if input1 == "1":
     num1 = int(input("digite o primeiro numero:"))
     num2 = int(input("digite o segundo numero:"))
 
-    resultado_da_soma = num1 + num2
+    resultado_da_soma = num1 + num2 
 
     print(f"o resultado e: {resultado_da_soma}")
 
@@ -14,13 +14,14 @@ elif input1 == "2":
     num1 = int(input("digite o primeiro numero:"))
     num2 = int(input("digite o segundo numero:"))
 
-    resultado_da_subtração = num1 - num2
+    resultado_da_subtração = num1 - num2 
 
     print(f"o resultado e: {resultado_da_subtração}")
 
 elif input1 == "3":
     num1 = int(input("digite o primeiro numero:"))
     num2 = int(input("digite o segundo numero:"))
+    
 
     resultado_da_multiplicação = num1 * num2
 
@@ -29,9 +30,25 @@ elif input1 == "3":
 elif input1 == "4":
     num1 = int(input("digite o primeiro numero:"))
     num2 = int(input("digite o segundo numero:"))
+    
 
-    resultado_da_divisão = num1 / num2
+    resultado_da_divisão = num1 / num2 
 
     print(f"o resultado e: {resultado_da_divisão}")
+
+elif input1 == "5":
+    num1 = int(input("digite o primeiro numero:"))
+    num2 = int(input("digite o segundo numero"))
     
-    
+    resultado_da_potencialização = num1 ** num2 
+
+    print(f"o resultado e: {resultado_da_potencialização}")
+
+elif input1 == "6":
+    input_loop = int(input("digite um numero para iniciar o loop:"))
+
+    for i in range(input_loop + 1):
+        print(i)
+
+elif input1 == "7":
+    print("saindo...")
