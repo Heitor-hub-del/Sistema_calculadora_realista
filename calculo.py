dd4530c30910c47bd0602dc3169e613b6074b052
Heitor-hub-del <heitortrader2023@gmail.com>
2026-01-25 10:58:34 -0300

    1.2 adições: adicionado dois novos operadores e um diferenciador de numeros e o reampeamento da tecla sair

--- calculo.py
+++ calculo.py
@@ -1,6 +1,6 @@
 print("bem vindo a calculadora realista!")
 
-input1 = input("qual calculo voce deseja fazer?  1.soma 2.subtração 3.multiplicação 4.divisão 5.potencialização,6. fazer um loop, 7.sair : ")
+input1 = input("qual calculo voce deseja fazer?  1.soma 2.subtração 3.multiplicação 4.divisão 5.potencialização,6. fazer um loop,  7. porcentagem, 8. fatorial, 9. diferenciar numeros, 10. sair: ")
 
 if input1 == "1":
     num1 = int(input("digite o primeiro numero:"))
@@ -51,4 +51,48 @@ elif input1 == "6":
         print(i)
 
 elif input1 == "7":
+    num = float(input("digite um numero:"))
+    porcentagem = float(input("digite a porcentagem:"))
+
+    resultado_da_porcentagem = (porcentagem / 100) * num
+
+    print(f"o resultado e: {resultado_da_porcentagem}")
+
+elif input1 == "8":
+
+    import math
+
+    num1 = int(input("digite o primeiro numero:"))
+    num2 = int(input("digite o segundo numero:"))
+
+    resultado_da_fatorial = math.factorial(num1) + math.factorial(num2)
+
+    print(f"o resultado e: {resultado_da_fatorial}")
+
+elif input1 == "9":
+
+    num1 = int(input("digite o primeiro numero:"))
+    num2 = int(input("digite o segundo numero:"))
+
+    numero_diferença = input("digite o < ou > ou = para saber qual numero sera comparado do maior ou do menor ou igual:")
+
+    if numero_diferença == "<":
+        if num1 < num2:
+            print(f"o numero {num1 or num2} e menor que {num2 or num1}")
+        else:
+            print(f"os numeros são iguais")
+
+    elif numero_diferença == ">":
+        if num1 > num2:
+            print(f"o numero {num1 or num2} e maior que {num2 or num1}")
+        else:
+            print(f"os numeros são iguais")
+
+    elif numero_diferença == "=":
+        if num1 == num2:
+            print(f"os numeros são iguais")
+        else:
+            print(f"os numeros são diferentes e não sao iguais")
+
+elif input1 == "9":
     print("saindo...")
